a80e8e7ea7f9386ba5a68872becb48e317025c23
Johnny Bae <jbaehova@gmail.com>
2026-09-12 07:14:07 +0900

    feat(ui): balance blue and red editorial accents

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -7,6 +7,7 @@ import "./consumption.css";
 import "./art-direction.css";
 import "./graphs.css";
 import "./readability.css";
+import "./accent-colors.css";
 import { Providers } from "./providers";
 import { AppShell } from "@/components/layout/app-shell";
 import { serverApiRequest } from "@/lib/api/server";
